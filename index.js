7a5408a0ec23135353afb1f8f9e8566ca2fe0eda
haykam821 <haykam821@users.noreply.github.com>
2018-05-28 19:24:35 -0400

    Code stuff

---
+++ index.js
@@ -0,0 +1,64 @@
+const config = require("./config.json");
+
+const djs = require("discord.js");
+const client = new djs.Client();
+client.login(config.token);
+
+const forEach = require("await-each");
+
+client.on("ready", () => {
+	const channel = client.channels.get(config.channel);
+	const guild = channel.guild;
+	const allRoles = guild.roles;
+
+	const cRoles = cleanRoles(config.roles, allRoles).filter(cRole => cRole);
+
+	forEach(cRoles, async role => {
+		if (!role) return;
+
+		const msg = await channel.send("", {
+			embed: {
+				title: role.name,
+				color: role.color,
+			},
+		});
+		await msg.react("✅");
+
+		const collector = msg.createReactionCollector((reaction, user) => {
+			return reaction.emoji.name === "✅" && user.id !== client.user.id;
+		});
+		collector.on("collect", async reaction => {
+			reaction.message.clearReactions();
+
+			const reactors = reaction.users.array().filter(reactor => reactor.id !== client.user.id);
+			updateMemberRoles(reactors, guild, reaction.message, cRoles);
+
+			reaction.message.react("✅");
+		});
+	});
+});
+
+function updateMemberRoles(reactors, guild, msg, cRoles) {
+	forEach(reactors, async reactor => {
+		const member = guild.members.get(reactor.id);
+		const roles = member.roles.filter(role => {
+			return cRoles.filter(cRole => cRole.id === role.id).length > 0;
+		});
+		member.addRole(FbNoID(guild.roles, msg.embeds[0].title), "Gave member new color role by their request.");
+		roles.forEach(role => member.removeRole(role), "Cleaning up color roles for member.");
+	});
+}
+
+function cleanRoles(roles, allRoles) {
+	return roles.map(role => FbNoID(allRoles, role)).sort((a, b) => {
+		return a.name.localeCompare(b.name);
+	});
+}
+
+// short for "Find by Name or ID"
+function FbNoID(collection, by) {
+	const byID = collection.find("id", by);
+	const byName = collection.find("name", by);
+
+	return byID ? byID : byName ? byName : undefined;
+}
